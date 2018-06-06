6db6292f12fd04d992f28658f889f38ca62c4ca1
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-06-06 18:34:32 +0200

    Initialize project

---
+++ AgentBack/src/main/java/com/example/AgentBack/AgentBackApplication.java
@@ -0,0 +1,12 @@
+package com.example.AgentBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgentBackApplication.class, args);
+	}
+}
