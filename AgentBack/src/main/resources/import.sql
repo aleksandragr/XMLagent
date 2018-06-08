747b8d1da28b61a0725fa984b7dae34e43d4463c
aleksandragr <grujica995@gmail.com>
2018-06-08 20:08:47 +0200

    import

--- AgentBack/src/main/resources/import.sql
+++ AgentBack/src/main/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO `xmlagent`.`agent` (`name`, `surname`, `approved`, `email`, `password`) VALUES ('Dragan','Dulic',1,'dragan.dulic1@gmail.com','odimidodjimi');
-INSERT INTO `xmlagent`.`agent` (`name`, `surname`, `approved`, `email`, `password`) VALUES ('Mika','Mikic',1,'donthavename@gmail.com','aaaa');
+INSERT INTO `xmlagent`.`agent` (`name`, `surname`, `approved`, `email`, `password`) VALUES ('Dragan','Dulic',1,'dragan.dulic1@gmail.com','vvvv');
+INSERT INTO `xmlagent`.`agent` (`name`, `surname`, `approved`, `email`, `password`) VALUES ('Mika','Mikic',1,'donthavename3478@gmail.com','aaaa');
