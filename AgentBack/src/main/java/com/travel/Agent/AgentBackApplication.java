55bf3b51ef2236979756f86be8b18c4f510e9f9a
dragandulic <35114903+dragandulic@users.noreply.github.com>
2018-06-08 23:44:26 +0200

    SOAP connection

--- AgentBack/src/main/java/com/travel/Agent/AgentBackApplication.java
+++ AgentBack/src/main/java/com/travel/Agent/AgentBackApplication.java
@@ -1,5 +1,7 @@
 package com.travel.Agent;
 
+import javax.xml.soap.*;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -9,5 +11,89 @@ public class AgentBackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(AgentBackApplication.class, args);
 		
+		
+		String soapEndpointUrl = "https://www.travel.com/accommodationws";
+        String soapAction = "https://www.travel.com/accommodation/setAccommodationStatusRequest";
+
+        callSoapWebService(soapEndpointUrl, soapAction);
+		
 	}
+	
+	private static void createSoapEnvelope(SOAPMessage soapMessage) throws SOAPException {
+        SOAPPart soapPart = soapMessage.getSOAPPart();
+
+        String myNamespace = "gs";
+        String myNamespaceURI = "https://www.travel.com/accommodation";
+
+        // SOAP Envelope
+        SOAPEnvelope envelope = soapPart.getEnvelope();
+        envelope.addNamespaceDeclaration(myNamespace, myNamespaceURI);
+        
+        
+            /*
+            Constructed SOAP Request Message:
+            <SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:myNamespace="http://www.webserviceX.NET">
+                <SOAP-ENV:Header/>
+                <SOAP-ENV:Body>
+                    <myNamespace:GetInfoByCity>
+                        <myNamespace:USCity>New York</myNamespace:USCity>
+                    </myNamespace:GetInfoByCity>
+                </SOAP-ENV:Body>
+            </SOAP-ENV:Envelope>
+            */
+
+        // SOAP Body
+        SOAPBody soapBody = envelope.getBody();
+        SOAPElement soapBodyElem = soapBody.addChildElement("setAccommodationStatusRequest", myNamespace);
+        SOAPElement soapBodyElem1 = soapBodyElem.addChildElement("free", myNamespace);
+        SOAPElement soapBodyElem2 = soapBodyElem.addChildElement("accommodationid", myNamespace);
+        soapBodyElem1.addTextNode("false");
+        soapBodyElem2.addTextNode("1");
+    }
+
+    private static void callSoapWebService(String soapEndpointUrl, String soapAction) {
+        try {
+            // Create SOAP Connection
+            SOAPConnectionFactory soapConnectionFactory = SOAPConnectionFactory.newInstance();
+            SOAPConnection soapConnection = soapConnectionFactory.createConnection();
+           
+            
+            
+            // Send SOAP Message to SOAP Server
+            SOAPMessage soapResponse = soapConnection.call(createSOAPRequest(soapAction), soapEndpointUrl);
+            
+            System.out.println("USPESNOOOOOOOOOOOOOOOOOOOOOOOOOO " + soapResponse);
+            
+            // Print the SOAP Response
+            System.out.println("Response SOAP Message:");
+            soapResponse.writeTo(System.out);
+            System.out.println();
+
+            soapConnection.close();
+        } catch (Exception e) {
+            System.err.println("\nError occurred while sending SOAP Request to Server!\nMake sure you have the correct endpoint URL and SOAPAction!\n");
+            e.printStackTrace();
+        }
+    }
+
+    private static SOAPMessage createSOAPRequest(String soapAction) throws Exception {
+        MessageFactory messageFactory = MessageFactory.newInstance();
+        SOAPMessage soapMessage = messageFactory.createMessage();
+
+        createSoapEnvelope(soapMessage);
+
+        MimeHeaders headers = soapMessage.getMimeHeaders();
+        headers.addHeader("SOAPAction", soapAction);
+
+        soapMessage.saveChanges();
+
+        /* Print the request message, just for debugging purposes */
+        System.out.println("Request SOAP Message:");
+        soapMessage.writeTo(System.out);
+        System.out.println("\n");
+
+        return soapMessage;
+    }
+	
+	
 }
